aa82a2c07dd554c08b31e6ec67eb60ddac696a67
shaka0919 <wangzhemh@outlook.com>
2020-06-29 11:10:14 +0800

    update git crendential

--- AuthDemo/Startup.cs
+++ AuthDemo/Startup.cs
@@ -36,7 +36,7 @@ namespace AuthDemo
                 app.UseDeveloperExceptionPage();
             }
 
-            //skip the cert check.
+            // skip the cert check.
             //app.UseHttpsRedirection();
 
             app.UseRouting();
